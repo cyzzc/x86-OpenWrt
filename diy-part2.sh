ba9551a14aa8f9ecc144e8d345b25371f56dad47
cyzzc <80954721+cyzzc@users.noreply.github.com>
2022-10-21 12:53:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,9 +23,9 @@ sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' target/linux/x86/Makefile
 
 ########### 更新lean的内置的smartdns版本 ###########
-# sed -i 's/1.2022.38/1.2022.38.1/g' feeds/packages/net/smartdns/Makefile
-# sed -i 's/1991a0b102e891f149647b162897bf4403f8f66c/51e1ba6897d40f078f4c32a70c1544865f1b4098/g' feeds/packages/net/smartdns/Makefile
-# sed -i 's/^PKG_MIRROR_HASH/#&/' feeds/packages/net/smartdns/Makefile
+sed -i 's/1.2022.38/1.2022.38.1/g' feeds/packages/net/smartdns/Makefile
+sed -i 's/1991a0b102e891f149647b162897bf4403f8f66c/51e1ba6897d40f078f4c32a70c1544865f1b4098/g' feeds/packages/net/smartdns/Makefile
+sed -i 's/^PKG_MIRROR_HASH/#&/' feeds/packages/net/smartdns/Makefile
 
 ########### 安装smartdns（必选）###########
 # git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
