8a7b780e3124c7b0716af40037946f7fae9b12c0
cyzzc <80954721+cyzzc@users.noreply.github.com>
2022-11-26 00:07:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,7 +12,4 @@
 
 # Add a feed source
 echo 'src-git nezha https://github.com/Erope/openwrt_nezha' >>feeds.conf.default
-echo 'src-git netspeedtest  https://github.com/sirpdboy/netspeedtest' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git cyzzc https://github.com/cyzzc/auto-cyzzc' >>feeds.conf.default
+echo 'src-git cyzzc https://github.com/cyzzc/cyzzc-package' >>feeds.conf.default
